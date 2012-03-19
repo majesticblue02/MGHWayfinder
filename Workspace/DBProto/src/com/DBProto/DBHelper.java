16bd67f05d8d43cf2de4e616e934981b106d324f
Kevin Brooks <majesticblue02@gmail.com>
2012-03-19 13:48:20 -0400

    modularized DBProtoActivity in prep for integration, added queries to DBHelper

--- Workspace/DBProto/src/com/DBProto/DBHelper.java
+++ Workspace/DBProto/src/com/DBProto/DBHelper.java
@@ -27,7 +27,7 @@ public class DBHelper extends SQLiteOpenHelper{
         this.myContext = context;
     }	
  
-    public void createDataBase() throws IOException{
+    public void createDataBase() throws IOException{										//creates the DB if it does not already exist in the application's db folder
     	
     	boolean dbExist = checkDB();
  
@@ -42,7 +42,7 @@ public class DBHelper extends SQLiteOpenHelper{
     	}
     }
  
-    private boolean checkDB(){
+    private boolean checkDB(){																//checks for the existance of the db in the program's db folder
  
     	SQLiteDatabase checkDB = null;
  
@@ -58,7 +58,7 @@ public class DBHelper extends SQLiteOpenHelper{
     	return checkDB != null ? true : false;
     }
  
-    private void copyDB() throws IOException{
+    private void copyDB() throws IOException{													//copies db from asset folder into the program's db folder
     	
     	String oFileName = DB_PATH + DB_NAME;
     	InputStream iStream = myContext.getAssets().open(DB_NAME);
@@ -99,29 +99,55 @@ public class DBHelper extends SQLiteOpenHelper{
  
 	}
  
-	public Cursor getFloorNodes(int floor){													//returns a recordset containing all nodes on a given floor
+	
+//////////////////SQL SELECT STATEMENTS/////////////////////////
+	
+	
+	public Cursor getFloorNodes(int floor){																//returns a recordset containing all nodes on a given floor
 		return db.rawQuery("SELECT * FROM tblNode WHERE nFloor = " + floor, null);
 	}
 	
-	public Cursor getNodeNeighbors(ArrayList<String> nodeIDs){										//returns a recordset containing all neighbor nodes of the string[] of node ID's
+	public Cursor getNodeNeighbors(ArrayList<String> nodeIDs){											//returns a recordset containing all neighbor nodes of the string[] of node ID's
 		String w = concatOr(nodeIDs, "mNode");
 				
 		return db.rawQuery("SELECT * FROM tblNeighbors WHERE mNode = " + w, null);
 	}
  
-	public Cursor getNodeType(int nFloor, ArrayList<String> nType){									//returns a recordset containing all nodes with a given nType[]
+	public Cursor getNodeType(int nFloor, ArrayList<String> nType){										//returns a recordset containing all nodes with a given nType[]
 		String w = concatOr(nType, "nType");
 
 		return db.rawQuery("SELECT * FROM tblNode WHERE nType = " + w, null);
 	}
 	
-	public int getNodeFloor(String nID){
+	public int getNodeFloor(String nID){																//returns the floor a given node is on
 		Cursor q = db.rawQuery("SELECT nFloor FROM tblNode WHERE nID = " + nID, null);
 		q.moveToFirst();
 		return q.getInt(4);
 	}
 	
-	private String concatOr(ArrayList<String> sIn, String cName){									//concats an array of strings into a single string seperated by OR's
+	public Cursor searchNode(String nID){																//searches for a particular node
+		return db.rawQuery("SELECT * FROM tblNode WHERE nID = " + nID, null);
+	}
+	
+	public Cursor getValidNodeTypes(int floor){															//returns valid node types in a given floor
+		return db.rawQuery("SELECT DISTINCT nType FROM tblNode WHERE nFloor = " + floor, null);
+	}
+	
+	public Cursor getDepNodes(String nDep){																//returns nodes in a given department
+		return db.rawQuery("SELECT * FROM tblNode WHERE nDep = " + nDep, null);
+	}
+	
+	public Cursor getFloorDeps(int floor){																//returns departments on a given floor
+		return db.rawQuery("SELECT DISTINCT nDep FROM tblNode WHERE nFloor = " + floor, null);
+	}
+	
+	public Cursor getDepFloors(String nDep){															//returns the floor(s) a department is on
+		return db.rawQuery("SELECT DISTINCT nFloor FROM tblNode WHERE nDep = " + nDep, null);
+	}
+	
+	
+	
+	private String concatOr(ArrayList<String> sIn, String cName){										//concats an array of strings into a single string seperated by OR's
 		String sOut = "";
 		
 		if (sIn.size() >= 1) {
--- Workspace/DBProto/src/com/DBProto/DBProtoActivity.java
+++ Workspace/DBProto/src/com/DBProto/DBProtoActivity.java
@@ -8,30 +8,42 @@ import android.app.Activity;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class DBProtoActivity extends Activity {
     /** Called when the activity is first created. */
+    
+    DBHelper db;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        Cursor RSa, RSn;
+        Hashtable<String, node> test;
+        ArrayList<String> keys;
+        String out = "";
+        TextView tv;
         
-        DBHelper db = new DBHelper(this.getApplicationContext());
+        db = new DBHelper(this.getApplicationContext());
         
-        Hashtable<String, node> aFloorHash = new Hashtable<String, node>();
-        ArrayList<String> nString = new ArrayList<String>();
-        node nodeM, nodeN;
-        String stringM, stringN;
+        tv = (TextView) this.findViewById(R.id.textArea);
         
-        CharSequence text = "got here";
-        Toast toast = Toast.makeText(this.getApplicationContext(), text, 2000);
+        initializeDB();
+        test = buildFloorNodes(1);
         
-
+        keys = new ArrayList<String>(test.keySet());
         
-        try { 
+        for(String key: keys){
+        	out += key + "\n";
+        }
+        
+        tv.setText(out);
+        
+    }
+    
+    private void initializeDB(){
+    	try { 
         	db.createDataBase();
         } 
         catch (IOException ioe) {
@@ -44,44 +56,53 @@ public class DBProtoActivity extends Activity {
         catch(SQLException sqle){
         	throw sqle;
         }
+    }
+    
+    private Hashtable<String, node> buildFloorNodes(int floor){					//Constructs and returns a hashtable of nodes, keyed by their nID's
+    	node nodeM;
+    	Hashtable<String, node> ht = new Hashtable<String, node>();
+    	ArrayList<String> nodeIDs = new ArrayList<String>();
+    	Cursor cursor;
+    	
+    	cursor = db.getFloorNodes(floor);										//Query DB for nodes on a given floor
+        cursor.moveToFirst();
         
-        
-        
-        RSa = db.getFloorNodes(1);											//Query DB for nodes on a given floor
-        RSa.moveToFirst();
-        
-        while(!RSa.isAfterLast()){											//Fill Hashtable for given floor
-        	nodeM = new node(	RSa.getString(1),								//nID
-        						RSa.getInt(2),									//x
-        						RSa.getInt(3),									//y
-        						RSa.getInt(4),									//nFloor
-        						RSa.getString(5),								//nType
-        						RSa.getString(6));								//nDep
+        while(!cursor.isAfterLast()){											//Fill Hashtable for given floor
+        	nodeM = new node(	cursor.getString(1),								//nID
+        						cursor.getInt(2),									//x
+        						cursor.getInt(3),									//y
+        						cursor.getInt(4),									//nFloor
+        						cursor.getString(5),								//nType
+        						cursor.getString(6));								//nDep
         	
-        	aFloorHash.put(nodeM.getNodeID(), nodeM);
-        	nString.add(nodeM.getNodeID());									//Build Array of NodeID's for finding neighbors
+        	ht.put(nodeM.getNodeID(), nodeM);									//add node to hashtable with corresponding nodeID as the key
+        	nodeIDs.add(nodeM.getNodeID());										//build string of nodeID's to query neighbor table
         	
-        	RSa.moveToNext();
+        	cursor.moveToNext();												//move to next record
         }
         
-        RSn = db.getNodeNeighbors(nString);									//Query for relevant neighbors
-        RSn.moveToFirst();
+        buildNeighborNodes(ht, nodeIDs);
+
+        return ht;
+    }
+    
+    private void buildNeighborNodes(Hashtable<String, node> ht, ArrayList<String> nIDs){
+    	String stringM, stringN;
+    	node nodeM, nodeN;
+    	Cursor cursor = db.getNodeNeighbors(nIDs);
+    	
+    	cursor.moveToFirst();
         
-        while(!RSn.isAfterLast()){
-        	stringM = RSn.getString(1);										//"Master" nodeID
-        	stringN = RSn.getString(2);										//"Neighbor" nodeID
+        while(!cursor.isAfterLast()){
+        	stringM = cursor.getString(1);							//"Master" nodeID
+        	stringN = cursor.getString(2);							//"Neighbor" nodeID
         	
-        	nodeM = aFloorHash.get(stringM);								//Retrieve master node from hashtable
-        	nodeN = aFloorHash.get(stringN);								//Retrieve neighbor node from hashtable
+        	nodeM = ht.get(stringM);								//Retrieve master node from hashtable
+        	nodeN = ht.get(stringN);								//Retrieve neighbor node from hashtable
         	
-        	nodeM.addNeighbor(nodeN);										//Add neighbor node to master node
+        	nodeM.addNeighbor(nodeN);								//Add neighbor node to master node
         	
-        	RSn.moveToNext();
-        	
+        	cursor.moveToNext();
         }
-        
-        
-        toast.show();
-        
     }
 }
