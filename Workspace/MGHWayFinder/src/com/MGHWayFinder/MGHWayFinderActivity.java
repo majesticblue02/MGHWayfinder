dcd0c937a9c583783f0946c846aedf414768e70c
BARNES_CALU <calumb@gmail.com>
2012-03-30 01:07:51 -0400

    test egit

--- Workspace/MGHWayFinder/src/com/MGHWayFinder/MGHWayFinderActivity.java
+++ Workspace/MGHWayFinder/src/com/MGHWayFinder/MGHWayFinderActivity.java
@@ -9,5 +9,6 @@ public class MGHWayFinderActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        //test git
     }
 }
