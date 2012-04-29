6591e1b66b4c52fe68885f6a28fbdbde129df673
Kevin Brooks <majesticblue02@gmail.com>
2012-04-29 11:24:37 -0400

    cleaned up and commented PathView

--- Workspace/MGHWayFinder/src/com/MGHWayFinder/PathView.java
+++ Workspace/MGHWayFinder/src/com/MGHWayFinder/PathView.java
@@ -23,27 +23,31 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Toast;
 
+/* This class is used to load a bitmap which corresponds with a given floor.
+ * It then draws a path determined by coordinates stored in the xArray and yArray ArrayLists.
+ * 
+ */
 public class PathView extends View{
 	
-	private ArrayList<Integer> xArray, yArray;									//arraylists used to hold x,y coords of node points
-	private int vWidth, vHeight;												//screen width and height (used for scaling)
-	private AssetManager am;
+	private ArrayList<Integer> xArray, yArray;									//ARRAYLISTS WHICH HOLD X AND Y COORDINATES FOR PLOTTING THE PATH
+	private int vWidth, vHeight;												//SCREEN HEIGHT AND WIDTH, AS MEASURED BY ONMEASURE
+	private AssetManager am;													//ASSETMANAGER FOR GRABBING ASSETS
 	
-	private Paint p = new Paint();												//paint used to stroke path
-	private Path path = new Path();												//display path
+	private Paint p = new Paint();												//PAINT USED TO STROKE PATH
+	private Path path = new Path();												//DISPLAY PATH
 	
-	private BitmapDrawable dMap;												//bitmap drawable used to draw to canvas
-	private Bitmap bMap;														//bitmap background
-	private BitmapFactory.Options op = new BitmapFactory.Options();;			//options used to define inputstream creation of bitmap
+	private BitmapDrawable dMap;												//BACKGROUND BITMAP DRAWN TO THE CANVAS
+	private Bitmap bMap;														//BACKGROUND BITMAP
+	private BitmapFactory.Options op = new BitmapFactory.Options();;			//OPTIONS USED TO DEFINE BITMAP STREAM
 	private InputStream is;
-	private Rect bounds;														//outer bounds of background bm
-	public Matrix matrix= new Matrix();											//matrix used to scale canvas
-	public Matrix savedMatrix = new Matrix();
+	private Rect bounds;														//OUTTER BOUNDS OF FLOOR MAP BITMAP
+	public Matrix matrix= new Matrix();											//MATRIX USED TO SCALE CANVAS
+	public Matrix savedMatrix = new Matrix();									//MATRIX USED TO STORE DURING INTERMEDIATE TRANSLATIONS
 	private float[] mValues = new float[9];										//USED TO RETRIEVE MATRIX VALUES (TRANSLATION & SCALE VALUES)
 	
-	final int ANIMATIONSTEP = 25;													//used for animation
-	final int ANIMATIONTOTAL = 500;
-	float transX, transY;
+	private final int ANIMATIONSTEP = 25;										//TIME (MILLIS) TAKEN IN BETWEEN EACH ANIMATION STEP
+	private final int ANIMATIONTOTAL = 500;										//TOTAL TIME TAKEN (MILLIS) TO MOVE THROUGH ANIMATION
+	private float transX, transY;												//INTERMITTENT STEPS IN THE TRANSLATION
 	
 	private final String[] source = {"","floor1color.png","floor2color.png"};		//STRNIG[] OF FLOOR PLAN FILE NAMES, STORED IN ORDER OF FLOOR	
 	
@@ -90,7 +94,6 @@ public class PathView extends View{
 		
 	}
 	
-	
 	//SETS INSTANCE ARRAYS, ASSET MANAGER, AND DRAWS THE VIEW
 	public void makePathView(ArrayList<Integer> xArray, ArrayList<Integer> yArray, int floor, AssetManager am) {
 
@@ -99,26 +102,26 @@ public class PathView extends View{
 		this.am = am;
 		
 		loadFloorMap(floor);
-		invalidate();
+		invalidate();																			//CALLED TO TELL VM TO REDRAW
 	}
 	
 	//CLEARS THE CURRENT PATH AND UPDATES IT
 	public void updatePath(ArrayList<Integer> x, ArrayList<Integer> y, int floor){			
-		xArray = null;																		//nulled to attempt to have gc remove old array objects
+		xArray = null;																			//nulled to attempt to have gc remove old array objects
 		yArray = null;
 		path.reset();
 		xArray = x;
 		yArray = y;
 		loadFloorMap(floor);
-		invalidate();										
+		invalidate();																			//CALLED TO TELL VM TO REDRAW
 	}
 	
 	//LOAD FLOOR PLAN BASED ON FLOOR NUMBER
 	private void loadFloorMap(int floor){
 		
 		try{
-			is = am.open(source[floor]);
-			bMap = BitmapFactory.decodeStream(is, null, op);
+			is = am.open(source[floor]);														//A BITMAP FACTORY IS USED IN CONJUNCTION WITH AN INPUTSTREAM TO HELP CONTROL THE SIZE OF THE IMAGE
+			bMap = BitmapFactory.decodeStream(is, null, op);									//THE BMF OPTIONS HELP CONTROL THE RESULTING IMAGE SIZE IN ANDROID
 			dMap = new BitmapDrawable(this.getResources(), bMap);
 			bounds = new Rect(0, 0, dMap.getIntrinsicWidth(), dMap.getIntrinsicHeight());
 			dMap.setBounds(bounds);
@@ -128,28 +131,20 @@ public class PathView extends View{
 		}
 	}
 	
-	//INITIALIZATION SCALE (FIT TO VIEWABLE AREA)
-	private void iniScale(){
-		if(((float)vWidth/(float)bounds.right) > ((float)vHeight/(float)bounds.bottom))
-			matrix.postScale(((float)vHeight/(float)bounds.bottom), ((float)vHeight/(float)bounds.bottom));
-		else
-			matrix.postScale(((float)vWidth/(float)bounds.right), ((float)vWidth/(float)bounds.right));		
-	}
-	
 	//ONDRAW CALL (CALLED BY VM TO DRAW TO SCREEN)
 	@Override
 	public void onDraw(Canvas canvas){
 		super.onDraw(canvas);
 		
 		vWidth = getMeasuredWidth();
-		vHeight = getMeasuredHeight();
+		vHeight = getMeasuredHeight();														//ONMEASURE CALLED BEFORE ONDRAW - VARIABLES USED FOR CENTERING THE SCREEN
 		
-		canvas.setMatrix(matrix);
+		canvas.setMatrix(matrix);															//APPLY THE TRANSFORMATION MATRIX TO THE CANVAS
 		
-		dMap.draw(canvas);														//draw the floor plan to the canvas
-		path.reset();															//reset the path
-		makePath();																//redraw the path
-		canvas.drawPath(path, p);
+		dMap.draw(canvas);																	//DRAW THE FLOOR PLAN TO THE CANVAS
+		path.reset();																		//RESET THE WALKING PATH
+		makePath();																			//REMAKE THE WALKING PATH
+		canvas.drawPath(path, p);															//DRAW THE PATH TO THE CANVAS
 	}
 	
 	//DRAW PATH USING COORDINATES
@@ -165,6 +160,8 @@ public class PathView extends View{
 			x = xArray.get(i);
 			y = yArray.get(i);
 			path.lineTo(x, y);
+			path.addCircle(x, y, 5, Path.Direction.CW);
+			path.moveTo(x, y);
 		}
 		
 		path.addCircle(x, y, 5, Path.Direction.CW);											//ADD A CIRCLE TO THE END NODE
@@ -195,7 +192,7 @@ public class PathView extends View{
 		return out;
 	}
 	
-	//MOVE VIEW TO CENTER OVER NODE n
+	//TRANSLATE VIEW TO CENTER OVER NODE n
 	public synchronized void setCenterPoint(Node n){
 		Point centerPoint;
 		float currentX, currentY;
@@ -206,26 +203,23 @@ public class PathView extends View{
 		if(getCenterPoint() != null){
 			centerPoint= getCenterPoint();
 			
-			matrix.getValues(mValues);
+			matrix.getValues(mValues);															//PULL OUT X AND Y TRANSFORMATION VALUES FROM MATRIX
 			currentX = mValues[Matrix.MTRANS_X];
 			currentY = mValues[Matrix.MTRANS_Y];
 			
 			currentX -= centerPoint.x;
-			currentY -= (centerPoint.y + 67);
+			currentY -= (centerPoint.y + 67);													//COMPENSATING FOR 67 PIXEL BUG
 			
 			
 			
-			transX = (nodeX - currentX) / (ANIMATIONTOTAL/ANIMATIONSTEP);
-			transY = (nodeY - currentY) / (ANIMATIONTOTAL/ANIMATIONSTEP);
+			transX = (nodeX - currentX) / (ANIMATIONTOTAL/ANIMATIONSTEP);						//SET EACH INTERMEDIATE X TRANSLATION
+			transY = (nodeY - currentY) / (ANIMATIONTOTAL/ANIMATIONSTEP);						//SET EACH INTERMEDIATE Y TRANSLATION
 			Thread ani = new Thread(animate, "translation animation");
 			ani.start();
-			
-			//matrix.postTranslate(transX, transY);
-			//invalidate();
 		}	
 	}
 	
-	//USED TO ANIMATE MOVEMENT
+	//USED TO ANIMATE TRANSLATION
 	private void step(){
 		matrix.postTranslate(transX, transY);
 		invalidate();
@@ -242,7 +236,7 @@ public class PathView extends View{
     	public void run(){
     		try {
     			for(int i = 0; i <= (ANIMATIONTOTAL/ANIMATIONSTEP); i++){
-    				Thread.sleep(ANIMATIONSTEP);
+    				Thread.sleep(ANIMATIONSTEP);												//SLEEP FOR EACH STEP, THEN TRANSLATE
     				handler.sendEmptyMessage(i);
     			}
 			} catch (InterruptedException e) {
