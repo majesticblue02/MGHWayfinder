cd605aeccd4e49df22c710fb2ad5e8b4a591b35e
Kevin Brooks <majesticblue02@gmail.com>
2012-04-29 11:59:17 -0400

    cleaned up and commented DBHelper

--- Workspace/MGHWayFinder/src/com/MGHWayFinder/DBHelper.java
+++ Workspace/MGHWayFinder/src/com/MGHWayFinder/DBHelper.java
@@ -27,8 +27,9 @@ public class DBHelper extends SQLiteOpenHelper{
     	super(context, DB_NAME, null, 1);
         this.myContext = context;
     }	
- 
-    public void createDataBase() throws IOException{										//creates the DB if it does not already exist in the application's db folder
+    
+    //MAIN LOADER LOOP - CHECK'S FOR DB'S EXISTANCE, LOADS FROM ASSETS IF NEEDED
+    public void createDataBase() throws IOException{
  
     	if(!checkDB()){
         	this.getReadableDatabase();
@@ -40,9 +41,9 @@ public class DBHelper extends SQLiteOpenHelper{
         	}
     	}
     }
- 
-    private boolean checkDB(){																//checks for the existance of the db in the program's db folder
- 
+    
+    //CHECKS FOR THE EXISTANCE OF THE DB IN THE PROGRAM'S DB FOLDER
+    private boolean checkDB(){
     	SQLiteDatabase checkDB = null;
  
     	try{
@@ -57,7 +58,8 @@ public class DBHelper extends SQLiteOpenHelper{
     	return checkDB != null ? true : false;
     }
  
-    private void copyDB() throws IOException{													//copies db from asset folder into the program's db folder
+    //COPIES DB FROM ASSET FOLDER INTO THE PROGRAM'S DB FOLDER
+    private void copyDB() throws IOException{
     	
     	String oFileName = DB_PATH + DB_NAME;
     	InputStream iStream = myContext.getAssets().open(DB_NAME);
@@ -73,13 +75,15 @@ public class DBHelper extends SQLiteOpenHelper{
     	oStream.close();
     	iStream.close();
     }
- 
+    
+    //USED TO OPEN A DB IF IT EXISTS
     public void openDataBase() throws SQLException{
         String myPath = DB_PATH + DB_NAME;
         
     	db = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
     }
- 
+    
+    //CLOSES THE DB
     @Override
 	public synchronized void close() {
     	    if(db != null)
@@ -101,26 +105,30 @@ public class DBHelper extends SQLiteOpenHelper{
 	
 //////////////////SQL SELECT STATEMENTS/////////////////////////
 	
-	public Cursor selectAllNids(){
+	//RETURNS ALL NIDS IN THE DB
+	public Cursor selectAllNids(){	
 		return db.rawQuery("SELECT nID FROM tblNode", null);
 	}
 	
-	public Cursor selectFloorNodes(int floor){																//returns a recordset containing all Nodes on a given floor
+	//RETURNS A RECORDSET CONTAINING ALL NODES ON A GIVEN FLOOR
+	public Cursor selectFloorNodes(int floor){
 		return db.rawQuery("SELECT * FROM tblNode WHERE nFloor = " + floor, null);
 	}
 	
-	public Cursor selectNodeNeighbors(String NodeID){											//returns a recordset containing all neighbor Nodes of the string[] of Node ID's
+	//RETURNS A RECORDSET CONTAINING ALL NEIGHBOR NODES OF THE GIVEN NODEID
+	public Cursor selectNodeNeighbors(String NodeID){
 		String w = "'" + NodeID + "'";
 		return db.rawQuery("SELECT * FROM tblNeighbors WHERE mNode = " + w, null );
 	}
  
-	public Cursor selectNodeType(int nFloor, ArrayList<String> nType){										//returns a recordset containing all Nodes with a given nType[]
+	//RETURNS A RECORDSET CONTAINING ALL OF THE NODES WITH A GIVEN NTYPE[]
+	public Cursor selectNodeType(int nFloor, ArrayList<String> nType){
 		String w = concatOr(nType, "nType");
-
 		return db.rawQuery("SELECT * FROM tblNode WHERE nType = " + w, null);
 	}
 	
-	public int getNodeFloor(String nID){																//returns the floor a given Node is on
+	//RETURNS THE FLOOR A GIVEN NODE IS ON
+	public int getNodeFloor(String nID){
 		int out;
 		Cursor q = db.rawQuery("SELECT nFloor FROM tblNode WHERE nID = '" + nID + "'", null);
 		q.moveToFirst();
@@ -129,27 +137,8 @@ public class DBHelper extends SQLiteOpenHelper{
 		return out;
 	}
 	
-	public Cursor selectNode(String nID){																//searches for a particular Node
-		return db.rawQuery("SELECT * FROM tblNode WHERE nID = '" + nID + "'", null);
-	}
-	
-	public Cursor selectValidNodeTypes(int floor){															//returns valid Node types in a given floor
-		return db.rawQuery("SELECT DISTINCT nType FROM tblNode WHERE nFloor = " + floor, null);
-	}
-	
-	public Cursor selectDepNodes(String nDep){																//returns Nodes in a given department
-		return db.rawQuery("SELECT * FROM tblNode WHERE nDep = '" + nDep + "'", null);
-	}
-	
-	public Cursor selectFloorDeps(int floor){																//returns departments on a given floor
-		return db.rawQuery("SELECT DISTINCT nDep FROM tblNode WHERE nFloor = " + floor, null);
-	}
-	
-	public Cursor selectDepFloors(String nDep){															//returns the floor(s) a department is on
-		return db.rawQuery("SELECT DISTINCT nFloor FROM tblNode WHERE nDep = '" + nDep + "'", null);
-	}
-	
-	public Cursor selectInterFloor(int floorA, int floorB){												//returns connecting relationships between floors A and B										
+	//RETURNS CONNECTING RELATIONSHIPS BETWEEN FLOORS A AND B
+	public Cursor selectInterFloor(int floorA, int floorB){									
 		return db.rawQuery(	"SELECT mNode, nNode " +
 							"FROM tblInterFloor " +
 							"INNER JOIN tblNode AS tblmNode " +
@@ -162,6 +151,7 @@ public class DBHelper extends SQLiteOpenHelper{
 	
 ////////////////////////////////PROGRAM METHODS////////////////////////////////////
 	
+	//RETURNS AN ARRAYLIST OF VALID NIDS
 	public ArrayList<String> getAllNids(){
 		ArrayList<String> out = new ArrayList<String>();
 		Cursor cursor;
@@ -179,23 +169,23 @@ public class DBHelper extends SQLiteOpenHelper{
 		return out;
 	}
 	
-	
-	public Hashtable<String, Node> buildFloorNodes(int floor){					//Constructs and returns a hashtable of Nodes, keyed by their nID's
+	//CONSTRUCTS AND RETURNS A HASHTABLE OF ALL NODES AND THEIR RELATIONSHIPS ON A GIVEN FLOOR
+	public Hashtable<String, Node> buildFloorNodes(int floor){
     	Node NodeM;
     	Hashtable<String, Node> ht = new Hashtable<String, Node>();
     	ArrayList<String> NodeIDs = new ArrayList<String>();
     	Cursor cursor;
     	
-    	cursor = this.selectFloorNodes(floor);										//Query DB for Nodes on a given floor
+    	cursor = this.selectFloorNodes(floor);									//Query DB for Nodes on a given floor
         cursor.moveToFirst();
         
         while(!cursor.isAfterLast()){											//Fill Hashtable for given floor
-        	NodeM = new Node(	cursor.getString(1),								//nID
-        						cursor.getInt(2),									//x
-        						cursor.getInt(3),									//y
-        						cursor.getInt(4),									//nFloor
-        						cursor.getString(5),								//nType
-        						cursor.getString(6));								//nDep
+        	NodeM = new Node(	cursor.getString(1),							//nID
+        						cursor.getInt(2),								//x
+        						cursor.getInt(3),								//y
+        						cursor.getInt(4),								//nFloor
+        						cursor.getString(5),							//nType
+        						cursor.getString(6));							//nDep
         	
         	ht.put(NodeM.getNodeID(), NodeM);									//add Node to hashtable with corresponding NodeID as the key
         	NodeIDs.add(NodeM.getNodeID());										//build string of NodeID's to query neighbor table
@@ -208,6 +198,32 @@ public class DBHelper extends SQLiteOpenHelper{
         return ht;
     }
     
+	//CONSTRUCTS INTERFLOOR RELATIONSHIPS
+    public void buildInterFloor(int floorA, int floorB, Hashtable<String, Node> ht){
+    	String stringM, stringN;
+    	Node nodeM, nodeN;
+    	Cursor cursor = selectInterFloor(floorA, floorB);
+    	
+    	cursor.moveToFirst();
+    	
+    	while(!cursor.isAfterLast()){
+    		stringM = cursor.getString(0);
+    		stringN = cursor.getString(1);
+    		
+    		nodeM = ht.get(stringM);								//Retrieve master Node from hashtable
+        	nodeN = ht.get(stringN);								//Retrieve neighbor Node from hashtable
+        	
+        	nodeM.addNeighbor(nodeN);								//Add neighbor Node to master Node
+        	
+        	cursor.moveToNext();
+    	}
+    	cursor.close();
+    }
+
+    
+////////////INTERNAL METHODS//////////////////////////
+    
+    //CONSTRUCTS RELATIONSHIPS GIVEN A TABLE OF NODES AND AN ARRAYLIST OF THEIR NEIGHBOR NIDS
     protected void buildNeighborNodes(Hashtable<String, Node> ht, ArrayList<String> nIDs){
     	String stringM, stringN;
     	Node NodeM, NodeN;
@@ -232,37 +248,9 @@ public class DBHelper extends SQLiteOpenHelper{
             cursor.close();
     	}
     }
-    
-    public void buildInterFloor(int floorA, int floorB, Hashtable<String, Node> ht){
-    	String stringM, stringN;
-    	Node nodeM, nodeN;
-    	Cursor cursor = selectInterFloor(floorA, floorB);
-    	
-    	cursor.moveToFirst();
-    	
-    	while(!cursor.isAfterLast()){
-    		stringM = cursor.getString(0);
-    		stringN = cursor.getString(1);
-    		
-    		nodeM = ht.get(stringM);								//Retrieve master Node from hashtable
-        	nodeN = ht.get(stringN);								//Retrieve neighbor Node from hashtable
-        	
-        	nodeM.addNeighbor(nodeN);								//Add neighbor Node to master Node
-        	
-        	cursor.moveToNext();
-    	}
-    	
-    	cursor.close();
-    	
-    }
 	
-	
-	
-	
-	
-////////////INTERNAL METHODS//////////////////////////
-	
-	private String concatOr(ArrayList<String> sIn, String cName){										//concats an array of strings into a single string seperated by OR's
+    //CONCATS AN ARRAYLIST OF STRING VARIABLES SEPERATED BY "OR"S
+	protected String concatOr(ArrayList<String> sIn, String cName){
 		String sOut = "";
 		
 		if (sIn.size() >= 1) {
