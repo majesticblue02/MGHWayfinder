dcea10a7639aeb4a96d0e401226f1729fce3e80b
Calum Barnes <calumb@gmail.com>
2012-05-16 01:54:34 -0400

    fixed centering on first point (needed to be slowed down for long routes on slow devices, from 500 to 2000). added some labels too

--- Workspace/MGHWayFinder/src/com/MGHWayFinder/PathDrawActivity.java
+++ Workspace/MGHWayFinder/src/com/MGHWayFinder/PathDrawActivity.java
@@ -480,7 +480,7 @@ public class PathDrawActivity extends ListActivity implements OnTouchListener{
 	Runnable centerOnLoad = new Runnable(){
    	public void run(){
    		try {
-   				Thread.sleep(1000);
+   				Thread.sleep(2000);
    				handler.sendEmptyMessage(0);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
